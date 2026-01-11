a1d0146c0dcca8c7ec1189be07f3c5bb182130ec
Sunil234554321 <sunilsarjal2@gmail.com>
2026-01-11 08:40:43 +0530

    Add home page

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Find Rooms</h1>
+      <p>Room finding app made with Next.js</p>
+
+      <a href="/rooms">View Available Rooms</a>
+    </main>
+  );
+}
